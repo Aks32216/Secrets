4ce93144c82e23e6b83b216f493f32ce89d17c9e
Aks32216 <amishsingh568@gmail.com>
2021-12-25 11:55:22 +0530

    2nd Level Security

---
+++ app.js
@@ -0,0 +1,72 @@
+const express=require("express");
+const ejs=require("ejs");
+const bodyParser=require("body-parser");
+const mongoose=require("mongoose");
+const encrypt=require("mongoose-encryption");
+
+const app=express();
+
+app.use(express.static("public"));
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+
+mongoose.connect("mongodb://127.0.0.1:27017/userDB",{useNewUrlParser:true});
+
+const userSchema=new mongoose.Schema({
+    email:String,
+    password:String
+});
+
+const secret="secretsaresecretsuntillrevealed.";
+userSchema.plugin(encrypt, { secret: secret,encryptedFields: ['password']});
+
+const User=new mongoose.model("User",userSchema);
+
+app.get("/",(req,res)=>{
+    res.render("home");
+})
+
+app.route("/login")
+.get((req,res)=>{
+    res.render("login");
+})
+.post((req,res)=>{
+    const emailAuth=req.body.username;
+    const passwordAuth=req.body.password;
+    User.findOne({email:emailAuth},(err,foundUser)=>{
+        if(err)
+            console.log(err);
+        else{
+            if(foundUser)
+            {
+                if(foundUser.password === passwordAuth)
+                {
+                    res.render("secrets");
+                }
+            }
+        }
+    })
+});
+
+app.route("/register")
+.get((req,res)=>{
+    res.render("register");
+})
+.post((req,res)=>{
+    const newEmail=req.body.username;
+    const newPassword=req.body.password;
+    const newUser=new User({
+        email:newEmail,
+        password:newPassword
+    })
+    newUser.save((err)=>{
+        if(err)
+            console.log(err);
+        else
+            res.render("secrets");
+    });
+});
+
+app.listen(3000,()=>{
+    console.log("server started sucessfully");
+})
